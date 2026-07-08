8c90bfabc29648028e622d083715bbb9827b1dc1
andres12234 <102494230+andres12234@users.noreply.github.com>
2026-07-07 20:09:27 -0500

    update

--- obj/Debug/net9.0-windows/GymBookingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/GymBookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymBookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2e35600b8c7e8d0159faa9d931ed37a945bd4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymBookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymBookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
